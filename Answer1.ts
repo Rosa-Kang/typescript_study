bc65dccd8de0fa1aabb5abd1a9e92d034919713b
Rosa Kang <rosakang@Rosas-MacBook-Air.local>
2025-05-26 12:52:30 -0400

    add quests from chapter2

--- Answer1.ts
+++ Answer1.ts
@@ -1,3 +1,4 @@
+// 1강
 // 문제 1. 다음 변수들의 타입을 지정해주세요
 let userName:string;
 let userAge:number;
@@ -50,5 +51,67 @@ function isPrimitive(value: unknown): boolean {
    typeof value === 'symbol' ||
    typeof value === 'bigint'
 }
+
+// 2강
+// 문제 1.아래 객체를 보고 user의 타입을 작성하세요 
+let user: {name: string; age?: number; isAdmin: boolean} = {
+  name: "Alice",
+  isAdmin: true,
+};
+
+user={
+  name: "Bob",
+  age:40,
+  isAdmin: false,
+} 
+
+// 문제2. 읽기 전용(readonly) 배열을 생성하고, 배열에 직접 값을 추가하거나 변경하려고 하면 오류가 발생해야 합니다.
+// 숫자만 담을 수 있는 읽기 전용 배열을 작성하세요.
+let numbers: readonly number[] = [1,2,3]
+
+// 문제3. 주어진 문제 1,2 번을 푸시오 
+// 1. 상품 이름과 가격만을 포함하는 새로운 배열을 생성하는 함수를 작성하세요.
+// 2. 재고가 있는 상품만 포함하는 배열을 반환하는 함수를 작성하세요.
+interface Product {
+  name: string;
+  price: number;
+  inventory: boolean
+}
+
+function itemsArray(products: Product[]): { name: string; price: number }[] {
+    return products.map(product => ({
+      name: product.name,
+      price: product.price
+    }));
+}
+
+function inventoryArray(products: Product[]): Product[] {
+    return products.filter(product => product.inventory);
+}
+
+// 문제 4. 사용자 정보를 업데이트하는 함수를 작성하세요. 나이가 제공되지 않으면 기본값으로 18을 사용하세요
+function updateUser(user:{name: string; age?: number}) : {name: string; age: number} {
+   return {...user, age: user.age ?? 18};
+}
+
+// 문제5. 아래와 같은 데이터 구조를 사용하여 특정 카테고리에 해당하는 상품의 이름을 출력하는 함수를 작성하세요.
+// proudcts 타입정의  필요 
+const products = [
+  { name: "Laptop", price: 1000, category: "Electronics" },
+  { name: "Shoes", price: 50, category: "Fashion" },
+  { name: "Book", price: 20 },
+];
+
+//매개변수, 리턴 타입 정의 필요
+function getProductsByCategory(category: string): string[] {
+   const sortedProducts = products.filter((product) => product.category === category);
+  return sortedProducts.map((product) => product.name);
+}
+
+// 테스트 코드
+console.log(getProductsByCategory("Electronics")); // ["Laptop"]
+console.log(getProductsByCategory("Fashion")); // ["Shoes"]
+console.log(getProductsByCategory("Books")); // []
+
 
 // 제가 시차를 잘못 계산해서 하루치 스터디를 제출 못했습니당 TT : 늦게나마 제출합니다~!
--- Answer2.ts
+++ Answer2.ts
@@ -162,3 +162,4 @@ const user5: AdminUser = {
     name: 'Terious',
     role: 'owner'
 }
+
